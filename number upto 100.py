46ebe1438e7e838857d937858de84c1a5a4b23e0
Daivik Trivedi <62508429+daivik007@users.noreply.github.com>
2020-05-28 16:15:10 +0530

    Create number upto 100.py
    this is a program that tells you that the number you have entered is greater than 100 or not.

---
+++ number upto 100.py
@@ -0,0 +1,8 @@
+while(True):
+    inp = int(input("enter a number\n"))
+    if inp>=100:
+        print("congrats you hae entered a number greater or equal to 100\n")
+        break
+    else:
+        print("try again!\n")
+        continue
